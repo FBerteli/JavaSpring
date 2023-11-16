3fcba0b072cb2b4da59a8cdd243a22436b56a9bf
Felipe Berteli <112442989+FBerteli@users.noreply.github.com>
2023-11-16 16:44:47 -0300

    Add files via upload
    Inicio do projeto

---
+++ exame-chuunin/src/main/java/br/com/felipeberteli/github/examechuunin/ExameChuuninApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipeberteli.github.examechuunin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameChuuninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameChuuninApplication.class, args);
+	}
+
+}
---
+++ exame-chuunin/src/test/java/br/com/felipeberteli/github/examechuunin/ExameChuuninApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipeberteli.github.examechuunin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameChuuninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
